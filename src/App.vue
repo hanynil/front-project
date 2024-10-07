123a92a30b80e5e0ee0d44197f919cb7ebf9c440
hanynil <hany.mozaffary@gmail.com>
2024-10-07 17:30:36 +0330

    update: i changed lots of things and also add a components which display time and date

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <currentDateTime/>
+</div>
+</template>
+
+<script>
+    import UpdateDateTime from './frontstation-practic/pr-1/components/UpdateDateTime.vue';
+import currentDateTime from './frontstation-practic/pr-1/components/UpdateDateTime.vue';
+    export default{
+        components:{
+            UpdateDateTime,
+        },
+
+    };
+</script>
---
+++ src/frontstation-practic/pr-1/components/UpdateDateTime.vue
@@ -0,0 +1,32 @@
+<script>
+    export default{
+          
+        data(){
+            return{
+                currentDateTime: "",
+            }; 
+        },
+        methods:{
+            updateDateTime(){
+                const now = new Date();
+                this.currentDateTime = now.toLocaleString();        
+            },
+        },
+        mounted(){
+            this.updateDateTime();
+            this.intervalId = setInterval(this.updateDateTime,1000);
+        },
+        beforeDestroy(){
+            clearInterval(this.intervalId);
+        },
+
+    };
+</script>
+
+<template>
+ <div>
+    <h1>زمان فعلی :</h1>
+    <span id="datetime">{{ currentDateTime }}</span>
+ </div>
+</template>
+
